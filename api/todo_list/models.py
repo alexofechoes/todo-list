2c11fd21086bdba00ea34a256ffcd00f0df08497
Alexander Bukharin <alexawm182@gmail.com>
2019-02-28 19:35:24 +0300

    add str method for Todo model

--- api/todo_list/models.py
+++ api/todo_list/models.py
@@ -6,3 +6,6 @@ class Todo(models.Model):
     name = models.CharField(max_length=200)
     is_open = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}'.format(self.name)
